816cb4e3b3256f946ebf3e110baae5f814160434
Sadia Afrin Mim <sadia.csedu@gmail.com>
2026-05-15 10:59:28 -0400

    rule based decision making

---
+++ Programming Artifacts/loan.py
@@ -0,0 +1,44 @@
+import sys
+sex = sys.argv[1]
+race = sys.argv[2]
+income = sys.argv[3]
+
+# first case
+if sex == "male":
+	print ("1")
+elif race != "green":
+	if income == "0...50000":
+		print ("0")
+	else:
+		print ("1")
+else:
+	if income == "0...50000" or income == "50001...100000":
+		print ("0")
+	else:
+		print ("1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
